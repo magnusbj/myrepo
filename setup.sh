b1b78012965e3de2598122866f98a83102a4eb7a
magnusbj <magnusbj@gmail.com>
2014-09-09 19:27:57 +0000

    new version, based on course

--- setup.sh
+++ setup.sh
@@ -1,2 +1,11 @@
 sudo apt-get update
-sudo apt-get install emacs
+sudo apt-get install -y emacs
+sudo apt-get install git-core
+git config --global user.name "magnusbj"
+git config --global user.email "magnusbj@gmail.com"
+mkdir setup
+git clone git@github.com:magnusbj/myrepo ./setup
+chmod 755 ./setup/setup.sh
+git clone https://github.com/startup-class/dotfiles.git
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
