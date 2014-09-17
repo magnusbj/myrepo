e8634961aad4c6668ed2ac45378089e027069b3a
magnusbj <magnusbj@gmail.com>
2014-09-17 06:47:31 +0000

    Setup ready for test

--- setup.sh
+++ setup.sh
@@ -1,11 +1,22 @@
 sudo apt-get update
 sudo apt-get install -y emacs
-sudo apt-get install git-core
+sudo apt-get install -y git
 git config --global user.name "magnusbj"
 git config --global user.email "magnusbj@gmail.com"
-mkdir setup
-git clone git@github.com:magnusbj/myrepo ./setup
-chmod 755 ./setup/setup.sh
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+npm install -h jshint
+wget -q0- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+#setup dotfiles
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
 git clone https://github.com/startup-class/dotfiles.git
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
+
+#load webpage
+cd $HOME
+git clone https://github.com/magnusbj/compdrill.git
